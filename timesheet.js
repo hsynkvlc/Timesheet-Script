f039a757e86826c5efbe3fe444d7afb059950f99
hsynkvlc <105428586+hsynkvlc@users.noreply.github.com>
2023-01-30 09:00:07 +0100

    Create timesheet.js

---
+++ timesheet.js
@@ -0,0 +1,24 @@
+//This script fills timesheet automatically
+//Right click on the timesheet webpage click inspect, click console and paste to code there enter
+
+for (var i=0; i<5; i++){
+document.getElementById('PUNCH_TIME_1$'+ i).value = '9:00:00AM'
+}
+for (var i=0; i<5; i++){
+document.getElementById('PUNCH_TIME_2$'+ i).value = '12:00:00PM'
+}
+for (var i=0; i<5; i++){
+document.getElementById('PUNCH_TIME_3$'+ i).value = '12:15:00PM'
+}
+for (var i=0; i<5; i++){
+document.getElementById('PUNCH_TIME_9$'+ i).value = '5:00:00PM'
+}
+for (var i=0; i<5; i++){
+document.getElementById('ACT_DESCR$'+ i).value = 'Technology Implement& Maintain'
+}
+for (var i=0; i<5; i++){
+document.getElementById('USER_FIELD_2$'+ i).value = 'ON'
+}
+for (var i=0; i<5; i++){
+document.getElementById('USER_FIELD_1$'+ i).value = 'B'
+}
